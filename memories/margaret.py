05261a3c3e0f1c916d4d4edf828643c17802781f
Jordan Nelson <jnelsontn@gmail.com>
2017-04-13 18:17:36 -0500

    Can talk to each other. Need to add list to dictionary

---
+++ memories/margaret.py
@@ -0,0 +1,37 @@
+import pickle
+import sys
+
+class Margaret(object):
+
+	def __init__(self):
+		self.messages = list()
+
+		try:
+			self.messages = self.deserialize()
+		except FileNotFoundError:
+			pass
+
+	def serialize(self):
+		with open('messages', 'wb+') as f:
+			pickle.dump(self.messages, f)
+
+	def deserialize(self):
+		try:
+			with open('messages', 'rb+') as f:
+				self.messages = pickle.load(f)
+		except EOFError:
+			pass
+
+		return self.messages
+
+	def list_messages(self):
+		for key, note in enumerate(self.messages):
+			print(str(key) + " : " + str(note))
+
+	def runme(self):
+		self.messages.append(sys.argv[1:])
+		self.serialize()
+		self.list_messages()
+		
+margaret = Margaret()
+margaret.runme()
