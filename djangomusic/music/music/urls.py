2ba94bb2e647201c15c7b5601b058f1fc684a6cf
Jordan Nelson <jnelsontn@gmail.com>
2017-04-19 11:58:19 -0500

    completed exercise

--- djangomusic/music/music/urls.py
+++ djangomusic/music/music/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^$', include('history.urls')),
 	url(r'^history/', include('history.urls')),
     url(r'^admin/', admin.site.urls),
 ]
